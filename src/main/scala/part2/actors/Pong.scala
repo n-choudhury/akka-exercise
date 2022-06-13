043a5a7d7f1635b9fc1bea86b353c43664917d99
Niloy <niloy@localhost.com>
2022-06-13 20:59:05 +0530

    Part2 - point 7 with solution.

--- src/main/scala/part2/actors/Pong.scala
+++ src/main/scala/part2/actors/Pong.scala
@@ -3,11 +3,11 @@ package part2.actors
 import akka.actor.{Actor, Props}
 import akka.event.Logging
 import part2.Main.{End, maxMessage}
-import part2.actors.{PongChild, WorkDone}
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-case object WorkDone
+case class WorkDone(num: Int)
 
 class Pong extends Actor {
   val log = Logging(context.system, this)
@@ -17,38 +17,46 @@ class Pong extends Actor {
   override def receive: Receive = {
     case "ping" =>
 //      log.info("ping")
-      sum += 1
-      if (sum < maxMessage) {
-        context.parent ! "pong"
-      }
-      else if (sum == maxMessage) context.parent ! End(sum)
-
-//      val pongChild = context.actorOf(Props[PongChild], s"pongChild_${System.nanoTime()}")
-//      pongChild ! "doWork"
-
-//    case WorkDone =>
-//      //      log.info(s"[WorkDone] sum = $sum")
 //      sum += 1
+
+    val selfActor = self
+      Future {
+        doWork()
+      }.map { num =>
+        selfActor ! WorkDone(num)
+      }
+      // Point 7
+//      Future {
+//        sum += doWork()
+//      }
 //      if (sum < maxMessage) {
 //        context.parent ! "pong"
-//      }
-//      else if (sum == maxMessage) context.parent ! End(sum)
+//      } else if (sum == maxMessage) context.parent ! End(sum)
+
+
+    case WorkDone(num) =>
+      sum += num
+      //      log.info(s"[WorkDone] sum = $sum")
+      if (sum < maxMessage) {
+        context.parent ! "pong"
+      } else if (sum == maxMessage) context.parent ! End(sum)
   }
 
-  //  def doWork(): Int = {
-  //    Thread sleep 1000
-  //    1
-  //  }
+    def doWork(): Int = {
+      Thread sleep 1000
+      1
+    }
 
 
   /**
    * ------------------------------- Point No. 7 explanation ----------------------------
    * Incrementing sum inside a Future block executes in a separate thread(outside of the scope of the actor)
-   * The thread sleeps for a second (until then sum does not get incremented) by the time all ping messages
-   * most likely been processed by the actor and hence could not get END.
+   * By the time all ping messages been processed by the actor, sum value won't be synchronized and will not
+   * get the final value to reach END.
    *
    * ------------------------------- Possible solutions: --------------------------------
-   * a. Using another actor to delegate heavy computation and avoid blocking the thread where the actor being executed.
+   * a. Performing heavy computation in a separate and avoid blocking the thread where the actor being executed.
+   *    mutating the state in a message handler (within actor scope)
    * b. Passing sum as method parameter in actor state instead of mutable sate.
    *
    */
